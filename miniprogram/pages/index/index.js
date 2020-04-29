a91cb8a0f7e00342168657ed3f3eaadf676c552c
周斌 <bin.zhou@k7.cn>
2020-04-29 19:54:20 +0800

    feat: 增加登录代码

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,4 +1,7 @@
 // pages/index/index.js
+
+const app = getApp()
+
 Page({
 
   /**
@@ -6,7 +9,8 @@ Page({
    */
   data: {
     search_default_content: "请输入要查找的菜谱名称",
-    tab_title: ["菜谱", "收藏", "兑换"]
+    tab_title: ["菜谱", "收藏", "兑换"],
+    hasUserInfo: false
   },
 
   addNewMenu: function()
@@ -15,12 +19,43 @@ Page({
       url: '/pages/add/add',
     })
   },
+
+  getUserInfo: function (e) {
+    console.log("getUserInfo")
+    console.log(e)
+    if('userInfo' in e.detail)
+    {
+      app.globalData.userInfo = e.detail.userInfo
+      this.setData({
+        hasUserInfo: true
+      })
+      // 写入数据库 UserInfo todo
+      console.log("写入数据库 UserInfo todo")
+    }
+    
+  },
   
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    if (app.globalData.userInfo) {
+      console.log("onLoad app.globalData.userInfo")
+      this.setData({
+        hasUserInfo: true
+      })
+    } else {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      console.log("app.userInfoReadyCallback")
+      console.log("app", app.globalData.userInfo)
 
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          hasUserInfo: true
+        })
+      }
+    }
   },
 
   /**
